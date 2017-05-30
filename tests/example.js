06032d9978af5256721690222932afe8e3d091e2
tmaslen <tom_maslen@hotmail.com>
2017-05-30 14:05:51 +0100

    setup mocha/chai

---
+++ tests/example.js
@@ -0,0 +1,8 @@
+const expect    = require("chai").expect;
+
+describe('Blobs', function() {
+  it('should list ALL blobs on /blobs GET', () => {
+  	expect( true ).to.equal( true );
+  });
+  it('should list a SINGLE blob on /blob/<id> GET');
+});
